9483a5cba0dbd20d9c6bc0e812b738736808078e
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2023-08-16 16:49:41 +0530

    updated code

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require("sequelize").Sequelize;
 const dotenv = require("dotenv").config();
 
-const { DB_NAME, DB_USERNAME, DB_PASSWORD } = dotenv.parsed;
+const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST } = dotenv.parsed;
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   dialect: "mysql",
-  host: "localhost",
+  host: DB_HOST,
 });
 
 module.exports = sequelize;
